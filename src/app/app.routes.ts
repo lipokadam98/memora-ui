867baa89875a1cbfd3afdfe981f16ad8b8eccf2a
lipoka <lipokadam98@gmail.com>
2026-01-22 22:59:35 +0100

    Add the Welcome component with the route and basic test setup.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {Welcome} from './components/welcome/welcome';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Welcome , pathMatch: 'full' },
+];
