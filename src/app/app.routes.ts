c30815530e4e8d09003eff304edbd8dac8a28b8d
lipoka <lipokadam98@gmail.com>
2026-01-23 23:18:12 +0100

    Remove redundant `pathMatch: 'full'` from root route definition.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import {Welcome} from './components/welcome/welcome';
 
 export const routes: Routes = [
-  { path: '', component: Welcome , pathMatch: 'full' },
+  { path: '', component: Welcome },
 ];
