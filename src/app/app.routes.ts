ab8c5cf41b3d32e16842023db1f1e3ed0502f0f4
lipoka <lipokadam98@gmail.com>
2026-02-25 14:10:47 +0100

    Adding Tailwind CSS to memora ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import {Welcome} from './components/welcome/welcome';
 
-export const routes: Routes = [
-  { path: '', component: Welcome },
-];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Navbar} from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('memora-ui');
