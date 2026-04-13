8597a785527e639fbd18dd09df9bd40b2dc3f105
lipoka <lipokadam98@gmail.com>
2026-04-13 20:35:26 +0200

    Replace progress bar with spinner during multimedia loading:
    - Use `MatProgressSpinner` instead of `MatProgressBar` for loading state.
    - Update gallery template to reflect layout adjustments for the new spinner.
    - Modify `gallery.ts` imports to include `MatProgressSpinner`.

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -1,16 +1,16 @@
 import { Component, inject, OnInit, ViewContainerRef } from '@angular/core';
 import { MultimediaStore } from '../multimedia/multimedia-store';
-import { MultimediaThumbnail } from '../multimedia/multimedia-thumbnail/multimedia-thumbnail';
 import { TranslatePipe } from '@ngx-translate/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
 import { Upload } from '../multimedia/upload/upload';
-import { MatProgressBar } from '@angular/material/progress-bar';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MultimediaThumbnail } from '../multimedia/multimedia-thumbnail/multimedia-thumbnail';
 
 @Component({
   selector: 'app-gallery',
-  imports: [MultimediaThumbnail, TranslatePipe, MatIcon, MatButton, MatProgressBar],
+  imports: [TranslatePipe, MatIcon, MatButton, MatProgressSpinner, MultimediaThumbnail],
   providers: [MultimediaStore],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css',
