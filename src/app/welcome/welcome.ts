95ef60d3e33619c397d139325c42853a4afae058
lipoka <lipokadam98@gmail.com>
2026-04-29 13:05:58 +0200

    Show loading spinner while multimedia items are being fetched in Welcome page.

--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { MultimediaStore } from '../multimedia/multimedia-store';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [MatProgressSpinner],
   templateUrl: './welcome.html',
   styleUrl: './welcome.css',
 })
