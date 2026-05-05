e9309d2b4f70de56a5d2db763bcbe15a358040c1
lipoka <lipokadam98@gmail.com>
2026-05-05 21:26:40 +0200

    Localize Welcome page:
    - Replace static title and subtitle with translation keys.
    - Update i18n files with new `welcome` translations for English and Hungarian.
    - Add `TranslatePipe` to Welcome component.

--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { MultimediaStore } from '../multimedia/multimedia-store';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-welcome',
-  imports: [MatProgressSpinner],
+  imports: [MatProgressSpinner, TranslatePipe],
   templateUrl: './welcome.html',
   styleUrl: './welcome.css',
 })
