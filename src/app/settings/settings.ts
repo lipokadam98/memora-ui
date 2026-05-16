cb6f7312fe052ee2d5e045d298e5e5a1f42aab76
lipoka <lipokadam98@gmail.com>
2026-05-16 20:01:59 +0200

    Implement Settings modal with dialog structure and localization:
    - Add `Settings` modal with template, dialog actions, and translation support.
    - Update i18n files for English and Hungarian with `settings.title` key.
    - Refactor `upload.html` by removing unnecessary `mat-typography` class.

--- src/app/settings/settings.ts
+++ src/app/settings/settings.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [
+    MatDialogTitle,
+    TranslatePipe,
+    MatDialogContent,
+    MatDialogActions,
+    MatButton,
+    MatDialogClose,
+  ],
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
