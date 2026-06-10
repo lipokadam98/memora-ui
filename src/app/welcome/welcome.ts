63d6db8b2d427f7b735dc3f41134472229abcc9d
lipoka <lipokadam98@gmail.com>
2026-06-10 14:03:16 +0200

    Enhance welcome page with empty state support and improved layouts
    - Add conditional rendering for welcome page empty states based on multimedia availability.
    - Extend i18n files (English, Hungarian) with translations for the empty state messages.
    - Update HTML templates to display empty state content and actions when applicable.
    - Refactor welcome component imports for streamlined functionality.

--- src/app/welcome/welcome.ts
+++ src/app/welcome/welcome.ts
@@ -2,10 +2,13 @@ import { Component, inject } from '@angular/core';
 import { MultimediaStore } from '../multimedia/multimedia-store';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { TranslatePipe } from '@ngx-translate/core';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-welcome',
-  imports: [MatProgressSpinner, TranslatePipe],
+  imports: [MatProgressSpinner, TranslatePipe, MatButton, RouterLink, MatIcon],
   templateUrl: './welcome.html',
   styleUrl: './welcome.css',
 })
