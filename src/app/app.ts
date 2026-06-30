22ed74b39c7e2a3d3f1cf6fddab802e6c23c3fc0
lipoka <lipokadam98@gmail.com>
2026-06-30 14:52:11 +0200

    Wrap app layout in a `mat-sidenav-container` for improved structure and adjust navbar styles accordingly.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
+import { MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, MatSidenavContainer, MatSidenavContent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
